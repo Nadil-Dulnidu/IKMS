374b366353bb1f1daf5fa0a1023820c99101eccb
Nadil Dulnidu <nzinlud963@gmail.com>
2026-01-05 02:43:32 +0530

    feat: Implement initial AI chat client with authentication and basic UI components.

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+const Header = () => {
+  return (
+    <div className="pb-4 flex justify-between">
+      <h1 className="text-2xl font-bold">IKMS</h1>
+      <div className="flex items-center">
+        <SignedIn>
+          <UserButton
+          />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton
+            mode="modal"
+          >
+            <Button variant="default" className="font-semibold pointer-events-auto">
+              Sign in
+            </Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
