b6d4d6b74ebd7a7b215c3e4b5240429dbd30a2d4
Nadil Dulnidu <nzinlud963@gmail.com>
2026-01-14 22:46:30 +0530

    change url enpoint

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -61,7 +61,7 @@ function ChatApp() {
   const transport = useMemo(
     () =>
       new DefaultChatTransport({
-        api: `${BACKEND_URL}/qa`,
+        api: `${BACKEND_URL}/api/chat`,
         headers: async () => {
           const token = await getToken({ template: "ikms" });
           return {
@@ -70,7 +70,7 @@ function ChatApp() {
         },
         body: () => {
           return {
-            thread_id: threadId
+            thread_id: threadId,
           };
         },
       }),
@@ -82,7 +82,7 @@ function ChatApp() {
     onError: (error) => {
       if (error instanceof Error) {
         const errorData = JSON.parse(error.message);
-        toast.error(`${errorData.detail}`,{
+        toast.error(`${errorData.detail}`, {
           action: {
             label: "Dismiss",
             onClick: () => toast.dismiss(),
@@ -94,7 +94,6 @@ function ChatApp() {
   });
 
   const handleSubmit = (message: PromptInputMessage) => {
-
     if (!isSignedIn) {
       toast.warning("Please sign in to continue.", {
         action: {
